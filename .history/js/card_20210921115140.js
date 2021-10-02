b35c63a07699facd094baf700afdc19cae23c85b
Robert Escalante <61357261+RobertAries28@users.noreply.github.com>
2021-10-01 22:51:50 -0500

    primer commit

---
+++ .history/js/card_20210921115140.js
@@ -0,0 +1,2 @@
+const cardTemplate= document.getElementById('video-card');
+const cardTemplate= document.getElementById('card-grid')
---
+++ .history/js/card_20210921120144.js
@@ -0,0 +1,8 @@
+const cardTemplate= document.getElementById('video-card');
+const cardGrid= document.getElementById('card-grid');
+
+
+for (let i=1; i<=12; i++){
+    const card= cardTemplate.content.cloneNode('card-grid')
+    cardGrid.appendChild(card)
+}
